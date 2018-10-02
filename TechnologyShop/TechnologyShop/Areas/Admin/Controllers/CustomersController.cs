9dd96fe108dd2922f1bbda2ee305b0f9559f7ac1
RaiWhale <lehoanghai22@gmail.com>
2018-10-02 16:40:43 +0700

    02-10

---
+++ TechnologyShop/TechnologyShop/Areas/Admin/Controllers/CustomersController.cs
@@ -0,0 +1,128 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using TechnologyShop.Models;
+
+namespace TechnologyShop.Areas.Admin.Controllers
+{
+    [Authorize]
+    public class CustomersController : Controller
+    {
+        private NewShopEntities db = new NewShopEntities();
+
+        // GET: Admin/Customers
+        public ActionResult Index()
+        {
+            return PartialView(db.Customers.ToList());
+        }
+
+        // GET: Admin/Customers/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Customer customer = db.Customers.Find(id);
+            if (customer == null)
+            {
+                return HttpNotFound();
+            }
+            return PartialView(customer);
+        }
+
+        // GET: Admin/Customers/Create
+        public ActionResult Create()
+        {
+            return PartialView();
+        }
+
+        // POST: Admin/Customers/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "Id,CustomerName,Gender,Birthday,Phone,Email,Password,Address,IsActive")] Customer customer)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Customers.Add(customer);
+                db.SaveChanges();
+                return Content("OK");
+            }
+
+            return PartialView(customer);
+        }
+
+        // GET: Admin/Customers/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Customer customer = db.Customers.Find(id);
+            if (customer == null)
+            {
+                return HttpNotFound();
+            }
+            return PartialView(customer);
+        }
+
+        // POST: Admin/Customers/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "Id,CustomerName,Gender,Birthday,Phone,Email,Password,Address,IsActive")] Customer customer)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(customer).State = EntityState.Modified;
+                db.SaveChanges();
+                return Content("OK");
+            }
+            return PartialView(customer);
+        }
+
+        // GET: Admin/Customers/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Customer customer = db.Customers.Find(id);
+            if (customer == null)
+            {
+                return HttpNotFound();
+            }
+            return PartialView(customer);
+        }
+
+        // POST: Admin/Customers/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Customer customer = db.Customers.Find(id);
+            db.Customers.Remove(customer);
+            db.SaveChanges();
+            return Content("OK");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
